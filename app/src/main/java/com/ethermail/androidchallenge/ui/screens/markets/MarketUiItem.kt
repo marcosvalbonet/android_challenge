448bccb39020ddf66ed85a190b67179fd5df1812
marcosvalbonet <marcos.valbonet@gmail.com>
2024-04-03 13:36:49 +0200

    Initial data classes

---
+++ app/src/main/java/com/ethermail/androidchallenge/ui/screens/markets/MarketUiItem.kt
@@ -0,0 +1,8 @@
+package com.ethermail.androidchallenge.ui.screens.markets
+
+data class MarketUiItem(
+    val exchangeId: String = "",
+    val rank: String = "",
+    val priceUsd: String = "",
+    val updated: String = ""
+)
